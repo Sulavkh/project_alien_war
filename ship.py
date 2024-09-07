1b754d03184a729739c2bb06ef8173e9cd03bff5
Sulavkh <sulove.kh@gmail.com>
2024-09-07 18:57:50 +0545

    refactoring code, adjusting speed of ship, respond to keypress

--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self, aw_game):
         """initialize ship and set starting position"""
         self.screen = aw_game.screen
+        self.settings = aw_game.settings
         self.screen_rect = aw_game.screen.get_rect()
 
         #load ship image
@@ -14,6 +15,24 @@ class Ship:
 
         #start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        #store float value for ship's horizontal position
+        self.x = float(self.rect.x)
+        
+        #movement flag;start with ship not moving.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """update the ship's position based on the movement flag."""
+        #Update ships x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
