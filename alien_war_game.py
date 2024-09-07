559e72952b40580cdebf13bee9285f69fde5e33a
Sulavkh <sulove.kh@gmail.com>
2024-09-07 15:23:19 +0545

    initial code for display and background to run game

--- alien_war_game.py
+++ alien_war_game.py
@@ -1,1 +1,37 @@
-print("Hello World")
+import sys
+
+import pygame
+
+class AlienWAR:
+    """Class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        #set background color
+        self.bg_color = (230, 230, 230)
+
+    def open_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #redraw the screen on each pass of loop
+            self.screen.fill(self.bg_color)
+            
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    aw = AlienWAR()
+    aw.open_game()
