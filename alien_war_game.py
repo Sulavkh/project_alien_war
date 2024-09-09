70688fed44891eef1d614b02c18d740119069ced
Sulavkh <sulove.kh@gmail.com>
2024-09-09 15:29:30 +0545

    display highscore and level

--- alien_war_game.py
+++ alien_war_game.py
@@ -91,6 +91,7 @@ class AlienWAR:
             # Reset the game statistics.
             self.stats.reset_stats()
             self.sb.prep_score()
+            self.sb.prep_level()
             self.game_active = True
 
             # Get rid of any remaining aliens and bullets.
@@ -169,12 +170,17 @@ class AlienWAR:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             #destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            #increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
